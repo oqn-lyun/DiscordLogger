5670d669ca9fd258f665b429bab5bfaf22606572
oqn-lyun <kirbybowl.hoi@gmail.com>
2022-05-17 23:11:12 +0900

    編集時処理を作成

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,8 @@
-import { Client, Message, MessageEmbed } from "discord.js";
+import { Client, MessageEmbed } from "discord.js";
 import { getConfig } from "./config";
 
 const config = getConfig();
 const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
-
-const buildEmbedMessage = (message: Message): MessageEmbed => {
-  const embed = new MessageEmbed()
-    .setAuthor({
-      name:
-        message.author.username + `（${message.member?.displayName ?? ""}）`,
-      iconURL: message.author.avatarURL() ?? undefined,
-    })
-    .setDescription(message.content);
-  for (const url of message.attachments.map((attachment) => attachment.url)) {
-    embed.addField("画像", url);
-  }
-  return embed;
-};
 
 client.once("ready", () => {
   const loggingChannel = client.channels.cache.get(config.loggingId);
@@ -58,7 +44,39 @@ client.on("messageCreate", async (message) => {
   await channel.send({ embeds: [embed] });
 });
 
-  await channel.send({ embeds: [buildEmbedMessage(message)] });
+client.on("messageUpdate", async (oldMessage, newMessage) => {
+  if (
+    !oldMessage.author ||
+    oldMessage.author.bot ||
+    !newMessage.author ||
+    newMessage.author.bot
+  ) {
+    return;
+  }
+  if (
+    oldMessage.channelId !== config.loggingId &&
+    newMessage.channelId !== config.loggingId
+  ) {
+    return;
+  }
+
+  const channel = client.channels.cache.get(config.outputId);
+  if (!channel?.isText()) {
+    return;
+  }
+  const embed = new MessageEmbed()
+    .setAuthor({
+      name:
+        newMessage.author.username +
+        `（${newMessage.member?.displayName ?? ""}）`,
+      iconURL: newMessage.author.avatarURL() ?? undefined,
+    })
+    .setFields([
+      { name: "編集前", value: oldMessage.content ?? "`削除`" },
+      { name: "編集後", value: newMessage.content ?? "`削除`" },
+    ]);
+
+  await channel.send({ embeds: [embed] });
 });
 
 client.login(config.token);
